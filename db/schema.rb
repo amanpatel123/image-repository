bb2dc3989192f14b03cf851d9d412403d7af5725
Aman Patel <patel1wq@uwindsor.ca>
2020-10-03 13:12:10 -0400

    added images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_214409) do
+ActiveRecord::Schema.define(version: 2020_10_03_170833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "filename", null: false
+    t.integer "slug", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
