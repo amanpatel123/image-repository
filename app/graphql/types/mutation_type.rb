76d8c6f029323bd5ab1ce3d9f75985518e39753f
Aman Patel <patel1wq@uwindsor.ca>
2020-12-17 11:06:12 -0500

    added signin mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,6 @@ module Types
     # TODO: remove me
     field :user_create, mutation: Mutations::User::Create, description: "Create a new user"
     field :image_create, mutation: Mutations::Image::Create, description: "Create a new Image"
-
+    field :user_sign_in, mutation: Mutations::User::SignIn, description: "Sign in a user"
   end
 end
---
+++ spec/graphql/mutations/user/sign_in_mutation_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Mutations
+  module User
+    RSpec.describe SignIn, type: :request do
+      describe 'resolver' do
+        let(:user) { create(:user, password: "password") }
+
+        # it 'Sign in a user' do
+        #   expect do
+        #     post '/graphql', params: { query: query, variables: variables(user) }
+        #   end.to change { ::User.count }.by(1)
+        # end
+        
+        context 'failure' do
+
+          it 'no account with the given email' do 
+            variables =  {
+              "input": {
+                "userEmail": "Notonsystem@gmail.com",
+                "password": "Letsgetit"
+              }
+            }
+
+            post '/graphql', params: { query: query, variables: variables }
+            json = JSON.parse(response.body)
+            data = json['data']['userSignIn']
+
+            expect(data).to include(
+              'message' => "Seems like you don't have an account with us",
+              'token'    => nil,
+              'user'     => nil
+            )
+          end
+
+          it 'invalid credentials' do 
+            variables =  {
+              "input": {
+                "userEmail": user.email,
+                "password": "Letsgetit"
+              }
+            }
+
+            post '/graphql', params: { query: query, variables: variables }
+            json = JSON.parse(response.body)
+            data = json['data']['userSignIn']
+
+            expect(data).to include(
+              'message' => "Invalid credentials",
+              'token'    => nil,
+              'user'     => nil
+            )
+          end
+        end
+      end
+
+      def query
+        <<~GQL
+          mutation signInUser($input: SignInUserInput!){
+            userSignIn(input: $input){
+              user{
+                fullName
+              }
+              message
+              token
+            }
+          }
+        GQL
+      end
+
+      def variables(user)
+        {
+          "input": {
+            "userEmail": user.email,
+            "password": user.password
+          }
+        }
+      end
+    end
+  end
+end
