b79d8210b138bb4734f19359d50bb96f7f1d3d31
Aman Patel <patel1wq@uwindsor.ca>
2020-12-25 04:59:06 -0500

    added description to images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_064320) do
+ActiveRecord::Schema.define(version: 2020_12_25_095638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_064320) do
     t.bigint "user_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "description"
     t.index ["user_id"], name: "index_images_on_user_id"
   end
 
